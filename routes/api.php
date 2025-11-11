02ec8ebac940d1a4ffc186f8f94221873a8b2a7b
Aleksander Szut <szut.aleksander+1@gmail.com>
2025-11-12 00:07:58 +0100

    feature(catalog): add categories with additional attributes for product

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/')->group(function () {
-    Route::prefix('backoffice/catalog')->group(base_path('src/Backoffice/Catalog/UI/Http/api.php'));
+//    Route::prefix('backoffice/catalog')->group(base_path('src/Backoffice/Catalog/UI/Http/api.php'));
 });
